22c3391e1f80dd36904df5a54501207f7992add5
tkim294 <tkim294@wisc.edu>
2021-12-31 13:00:06 -0800

    Slack Clone

---
+++ myscript.js
@@ -0,0 +1,39 @@
+// Fix Nav bar for min width 1024px
+var distance = $("#second-body-section").offset().top,
+  $window = $(window);
+
+$window.scroll(function () {
+  if (screen.width > 1024) {
+    if ($window.scrollTop() >= distance) {
+      // Your div has reached the top
+      $("#main-nav").addClass("is-fixed");
+      $("#main-nav-shadow-div").css(
+        "box-shadow",
+        "box-shadow: 0 4px 40px rgb(0 0 0 / 8%);"
+      );
+    } else {
+      $("#main-nav").removeClass("is-fixed");
+      $("#main-nav-shadow-div").css("box-shadow", "none");
+    }
+  }
+});
+
+// Swiper
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  speed: 400,
+  spaceBetween: 100,
+  loop: true,
+});
+
+function onClickAccordion(id) {
+  var button = document.getElementById(id);
+  if (button.className.indexOf("v-show") == -1) {
+    button.classList.add("v-show");
+  } else {
+    button.classList.remove("v-show");
+  }
+}
